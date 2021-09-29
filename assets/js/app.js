dcddaf12cc6ace414fff35fa1aa91d4483b7047b
Mat Lundin <matmll12@gmail.com>
2021-09-29 17:10:57 -0500

    added generate table, started save btn event listen, update sched from loc stor

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,30 +2,102 @@
 // WHEN I open the planner
 // THEN the current day is displayed at the top of the calendar
 // use moment() to grab day and hour, save to global var
-var time = moment().format('dddd - hA');
-console.log(time)
-$('#currentDay').text(time)
+var tableBodyEl = $('#tablebody')
+var time = moment();
+console.log(time.format('HHmm'))
+//format the moment here instead of the var so we can format it for comparisons later
+$('#currentDay').text(time.format('dddd - hA'))
 // WHEN I scroll down
 // THEN I am presented with timeblocks for standard business hours
 // html table generator? or create table based on array of objects?
 var sched = [
     {
-        time: 0900,
+        time: '0900',
+        displayTime: '9AM',
         text: ''
     },
     {
-        time: 1000,
+        time: '1000',
+        displayTime: '10AM',
         text: ''
     },
     {
-        time: 1100,
+        time: '1100',
+        displayTime: '11AM',
+        text: ''
+    },
+    {
+        time: '1200',
+        displayTime: '12PM',
+        text: ''
+    },
+    {
+        time: '1300',
+        displayTime: '1PM',
+        text: ''
+    },
+    {
+        time: '1400',
+        displayTime: '2PM',
+        text: ''
+    },
+    {
+        time: '1500',
+        displayTime: '3PM',
+        text: ''
+    },
+    {
+        time: '1600',
+        displayTime: '4PM',
+        text: ''
+    },
+    {
+        time: '1700',
+        displayTime: '5PM',
+        text: ''
+    },
+    {
+        time: '1800',
+        displayTime: '6PM',
         text: ''
     }
 ]
+// console.log(time.format('HHmm') > sched[1].time)
+
+//update sched to be whatever is in local storage
+function getSched(){
+    sched = localStorage.getItem('sched');
+};
+
+//generate the table rows
+sched.forEach(function(item,index){
+    var rowEl = $('<tr>').addClass(item.time.toString());
+    var timeEl = $('<td>').text(item.displayTime);
+    var textEl = $('<td>').text(item.text).addClass('text');
+    var saveEl = $('<td>').addClass('col-1');
+    var saveBtnEl = $('<button>').text('Save').addClass('save');
+    saveEl.append(saveBtnEl);
+    rowEl.append(timeEl);
+    rowEl.append(textEl);
+    rowEl.append(saveEl);
+    tableBodyEl.append(rowEl);
+});
 // WHEN I view the timeblocks for that day
 // THEN each timeblock is color coded to indicate whether it is in the past, present, or future
 // based on textContent or data-time? compare to time var
 // WHEN I click into a timeblock
+
+// click listener on container, then grab target, see if it is a save or a text
+//replaced with listeners on the .save and the .text, then we grab their parent class for the time
+// $('.container').click(function(event){
+//     // console.log('target = '+target);
+//     console.log('event.target = ' + event.target);
+    
+// })
+$('.save').click(function(event){
+    console.log('row class = ' + $(event.target).parent().parent().attr('class'));
+    
+})
 // THEN I can enter an event
 // https://stackoverflow.com/questions/28845037/input-text-value-into-a-div
 // event listener on the div that has a key listener loop that appends to .textContent
@@ -35,4 +107,4 @@ var sched = [
 // save entire sched array to local storage
 // WHEN I refresh the page
 // THEN the saved events persist
-// repopulate list/schedule with sched array
+// repopulate list/schedule with sched
